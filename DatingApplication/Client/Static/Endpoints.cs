6f9a3dc621d0454f5585203e60d1668b2902f5a4
Fabien Sundram <fabiensundram@gmail.com>
2024-01-30 16:45:21 +0800

    Endpoints Done

---
+++ DatingApplication/Client/Static/Endpoints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApplication.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string DatingAppUserEndpoint = $"{Prefix}/datingAppUsers";
+        public static readonly string UserProfilesEndpoint = $"{Prefix}/userProfiles";
+        public static readonly string MatchesEndpoint = $"{Prefix}/matches";
+        public static readonly string ChatsEndpoint = $"{Prefix}/chats";
+        s
+    }
+}
